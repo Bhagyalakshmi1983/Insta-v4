bb4384a766d9d1236569979dfb905fe59ddd2483
Bhagyalakshmi1983 <lakshmithota04@gmail.com>
2022-09-12 16:01:12 +0530

    Add the menu profile image at the right side of the header

--- Components/Header.js
+++ Components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Image from 'next/image';
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-
+import { MagnifyingGlassIcon,PlusCircleIcon, } from "@heroicons/react/24/outline";
+import { HomeIcon } from "@heroicons/react/24/solid";
 
 export default function Header() {
   return (
     <div>
-      <div className='flex items-center justify-between max-w-6xl'>
+      <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
         {/* Left */}
         <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
           <Image
@@ -30,8 +30,12 @@ export default function Header() {
           <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"/>
         </div>
         {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img src="./images/DBVL7306.JPG"  alt="user-image" className="h-10 rounded-full cursor-pointer"/>
+</div>
 
-        <h1>Right side</h1>
       </div>
 
 
